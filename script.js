58fd40d4e1862d5552632fb6b5aee4b761f57e14
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-08 10:22:46 +0200

    Add query selectors

---
+++ script.js
@@ -0,0 +1,6 @@
+const search = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const randomBtn = document.getElementById('random-btn');
+const searchTitle = document.getElementById('search-title');
+const mealsContainer = document.getElementById('meals-container');
+const pickedMeal = document.getElementById('picked-meal-container');
