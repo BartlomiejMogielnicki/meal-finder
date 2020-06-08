9b5319241b22469b81c896c5b63259135572970d
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-08 11:11:30 +0200

    Add Fetch API and show response results in DOM

--- script.js
+++ script.js
@@ -9,7 +9,35 @@ const pickedMeal = document.getElementById('picked-meal-container');
 // Search meals
 const searchMeals = (e) => {
     e.preventDefault();
-    console.log(search.value);
+
+    const input = search.value;
+
+    // Check if input is not empty
+    if (input) {
+        // Fetch data API
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+            .then(response => response.json())
+            .then(data => {
+                // Set title
+                searchTitle.innerHTML = `<h2>Search results for '${input}':</h2>`;
+                // Check if response is not empty
+                if (data.meals === null) {
+                    searchTitle.innerHTML = `<h2>There are no results for '${input}'. Please try again.</h2>`;
+                } else {
+                    // Show response results
+                    mealsContainer.innerHTML = data.meals.map(meal =>
+                        `<div class="meal">
+                            <img src="${meal.strMealThumb}" alt="${input} picture"/>
+                            <h3>${meal.strMeal}</h3>
+                        </div>`
+                    ).join('');
+                }
+            });
+        // Clear search input
+        search.value = "";
+    } else {
+        alert('Please enter your question')
+    }
 }
 
 // Add events
