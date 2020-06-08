fc6c2c37d9a73a40e351a39934c510a1f384f4ec
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-08 10:32:48 +0200

    Add form to index.html

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const search = document.getElementById('search-input');
-const searchBtn = document.getElementById('search-btn');
+const submit = document.getElementById('submit');
 const randomBtn = document.getElementById('random-btn');
 const searchTitle = document.getElementById('search-title');
 const mealsContainer = document.getElementById('meals-container');
 const pickedMeal = document.getElementById('picked-meal-container');
+
+
+// Search meals
+const searchMeals = (e) => {
+    e.preventDefault();
+    console.log(search.value);
+}
+
+// Add events
+submit.addEventListener('submit', searchMeals);
